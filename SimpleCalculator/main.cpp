fabef4431f07405358473b27790710a291c5dd08
Brent Perry <burentoperii@gmail.com>
2016-07-07 09:23:14 -0500

    Initial commit of SimpleCalculator.

--- SimpleCalculator/main.cpp
+++ SimpleCalculator/main.cpp
@@ -6,10 +6,26 @@
 //  Copyright © 2016 Brent Perry. All rights reserved.
 //
 
-#include <iostream>
+#include "../../../std_lib_facilities.h"
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main()
+{
+    string operation;
+    double val1, val2;
+    double result;
+    
+    cout << "Please enter an operator and two numbers separated by spaces (e.g. '+ 1 1'): ";
+    cin >> operation >> val1 >> val2;
+    
+    if (operation == "+")
+        result = val1 + val2;
+    else if (operation == "-")
+        result = val1 - val2;
+    else if (operation == "*")
+        result = val1 * val2;
+    else if (operation == "/")
+        result = val1 / val2;
+    
+    cout << result << "\n";
     return 0;
 }
